cf0beff61869ad3af7e7cc66117851fb33649261
Erik Dymbe <erik.dymbe@skyetec.no>
2025-03-10 14:54:44 +0100

    Modify next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,45 @@
 import type { NextConfig } from "next";
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = (phase: string): NextConfig => {
+  return {
+    output: "export",
+    basePath: phase === PHASE_PRODUCTION_BUILD ? "/base/path" : undefined,
+    rewrites:
+      phase === PHASE_DEVELOPMENT_SERVER
+        ? async () => {
+            return [
+              {
+                source: "/api/:path*",
+                destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+              },
+            ];
+          }
+        : undefined,
+    webpack(config) {
+      // Exclude SVGs from the default file loader
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      });
+
+      return config;
+    },
+    experimental: {
+      turbo: {
+        rules: {
+          "*.svg": {
+            loaders: ["@svgr/webpack"],
+            as: "*.js",
+          },
+        },
+      },
+    },
+  };
 };
 
 export default nextConfig;
